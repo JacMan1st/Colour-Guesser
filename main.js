f75aa3b83ee47a024547a43715ef4fad99a3d3ac
Jack Mears <jack.mears@ymail.com>
2024-01-10 16:30:32 +0000

    colour guesser

---
+++ main.js
@@ -0,0 +1,55 @@
+
+const colourGuess = document.getElementById("colourGuess");
+const newColrBtn = document.getElementById("newColrBtn");
+const correctMessage = document.getElementById("correctMessage");
+const score = document.getElementById("score");
+const allBoxes = document.querySelectorAll(".box");
+
+
+let newColour = null;
+let displayScore = 0;
+let guess = false;
+
+
+allBoxes.forEach((colourBox) => {
+    colourBox.addEventListener("click", () => {
+
+        if(guess){
+            if (colourBox.style.backgroundColor === newColour) {
+                correct();
+            } else {
+                colourBox.style.opacity = "0";
+            }
+        }
+    });
+});
+
+const correct = () => {
+    guess = false;
+    displayScore++
+    score.textContent = displayScore;
+    correctMessage.textContent = "Correct!";
+    // changed to forEach to make more robust and tidy
+    allBoxes.forEach((box) => {
+        box.style.backgroundColor = newColour;
+    });
+
+}
+
+const resetGame = () => {
+    guess = true;
+    correctMessage.textContent = "";
+    // same changes. seems to work the same, unsure if there is any disadvantage?
+    allBoxes.forEach((box) => {
+        box.style.opacity = "1";
+        box.style.backgroundColor = `rgb(${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)}, ${Math.round(Math.random() * 255)})`;
+    });
+    let elementGuess = allBoxes[Math.floor(Math.random() * allBoxes.length)]
+
+    newColour = elementGuess.style.backgroundColor;
+
+    colourGuess.textContent = newColour;
+}
+resetGame()
+
+newColrBtn.addEventListener("click", resetGame)
